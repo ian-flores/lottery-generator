994d5ab5dc2469ae0e5ec2ffe65589ef72b50311
ian-flores <iflores.siaca@hey.com>
2021-12-15 09:44:44 -0400

    Fixing color formatting

--- pkg/generators/generators.go
+++ pkg/generators/generators.go
@@ -8,24 +8,30 @@ import (
 	"github.com/fatih/color"
 )
 
+// Powerball generates the 5 numbers and the 1 special number for the powerball draw
 func Powerball() {
 	draw_numbers, special_number := contest_generator(5, 69, 26)
 	color.Cyan("Powerball Numbers")
 	printResults(draw_numbers, special_number)
 }
 
+// Traditional generates the 5 numbers and the 1 special number for the
+// traditional draw
 func Traditional() {
 	draw_numbers, special_number := contest_generator(5, 35, 10)
 	color.Cyan("Traditional Numbers")
 	printResults(draw_numbers, special_number)
 }
 
+// Pega generates the different sets of numbers for Pega 2, Pega 3 and Pega 4
 func Pega(pega_num int) {
 	draw_numbers, special_number := contest_generator(pega_num, 9, 0)
-	color.Cyan("Pega", pega_num)
+	fmt.Println("Pega", pega_num)
 	printResults(draw_numbers, special_number)
 }
 
+// contest_generator generates the numbers for any random contest. It is the
+// main function of the package.
 func contest_generator(draw_numbers_size int, draw_number_max int, special_number_max int) ([]int, int) {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
@@ -43,11 +49,12 @@ func contest_generator(draw_numbers_size int, draw_number_max int, special_numbe
 	return numbers, 0
 }
 
+// printResults prints the results of the contest
 func printResults(draw_numbers []int, special_number int) {
-	color.Green("Draw Numbers:", draw_numbers)
+	fmt.Println("Draw Numbers:", draw_numbers)
 
 	if special_number != 0 {
-		color.Red("Special Number:", special_number)
+		fmt.Println("Special Number:", special_number)
 	}
 	fmt.Println("")
 }
